dea897b464a6ca21dc7e894eef8d33ca3cb34c93
三咲智子 Kevin Deng <sxzz@sxzz.moe>
2024-03-08 22:42:22 +0800

    feat: add bin

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,11 +1,10 @@
 import { defineConfig } from 'tsup'
 
 export default defineConfig({
-  entry: ['./src/index.ts'],
-  format: ['cjs', 'esm'],
+  entry: ['./src/{index,cli-run}.ts'],
+  format: 'esm',
   target: 'node18',
   splitting: true,
-  cjsInterop: true,
   clean: true,
   dts: true,
   platform: 'node',
