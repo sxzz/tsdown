0051a49e92b4d45be942fa2b67a8a707ad1b5095
三咲智子 Kevin Deng <sxzz@sxzz.moe>
2024-06-26 02:34:42 +0800

    fix: cleanup outDir

--- src/features/clean.ts
+++ src/features/clean.ts
@@ -2,13 +2,17 @@ import { readdir, rm } from 'node:fs/promises'
 import path from 'node:path'
 import { globby } from 'globby'
 import { logger } from '../utils/logger'
+import { fsExists } from '../utils/fs'
 
 // clean cwd dir + patterns
 export async function cleanOutDir(
   cwd: string,
   patterns: string[],
 ): Promise<void> {
-  const files = (await readdir(cwd)).map((file) => path.resolve(cwd, file))
+  const files = []
+
+  if (await fsExists(cwd))
+    files.push(...(await readdir(cwd)).map((file) => path.resolve(cwd, file)))
 
   if (patterns.length) {
     files.push(...(await globby(patterns, { cwd, absolute: true })))
