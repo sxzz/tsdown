ff5b6176b9e50520343f02f9618a86e213bebeff
三咲智子 Kevin Deng <sxzz@sxzz.moe>
2024-07-18 06:54:13 +0800

    test: basic setup

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,3 +1,36 @@
-import { test } from 'vitest'
+import { rm } from 'node:fs/promises'
+import { beforeEach, expect, test } from 'vitest'
+import { getTestDir, testBuild } from './utils'
 
-test('simple', () => {})
+beforeEach(async () => {
+  const dir = getTestDir()
+  await rm(dir, { recursive: true }).catch(() => {})
+})
+
+test('basic', async () => {
+  const content = `console.log('Hello, world!')`
+  const { outputContent } = await testBuild({
+    'index.ts': content,
+  })
+  const out = await outputContent()
+  expect(out).matchSnapshot()
+  expect(out).contain(content)
+})
+
+{
+  const files = {
+    'index.ts': "export { foo } from './foo'",
+    'foo.ts': 'export const foo = 1',
+  }
+  test('esm import', async () => {
+    const { outputContent } = await testBuild(files)
+    const out = await outputContent()
+    expect(out).matchSnapshot()
+  })
+
+  test('cjs import', async () => {
+    const { getContent } = await testBuild(files, { args: ['--format', 'cjs'] })
+    const out = await getContent('index.js')
+    expect(out).matchSnapshot()
+  })
+}
---
+++ tests/utils.ts
@@ -0,0 +1,77 @@
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
+import { mkdir, readFile, writeFile } from 'node:fs/promises'
+import { execa } from 'execa'
+import { expect } from 'vitest'
+import { fdir } from 'fdir'
+
+const dirname = path.dirname(fileURLToPath(import.meta.url))
+const tmpDir = path.resolve(dirname, 'temp')
+
+const tsx = require.resolve('tsx/cli')
+const run = path.resolve(dirname, '../src/run.ts')
+
+export function getTestDir(): string {
+  const testName = getTestName()
+  return path.resolve(tmpDir, filenamify(testName))
+}
+
+export async function testBuild(
+  files: Record<string, string>,
+  {
+    args = [],
+    entry = 'index.ts',
+    config = false,
+    outDir = 'dist',
+  }: {
+    args?: string[]
+    entry?: string
+    config?: boolean
+    outDir?: string
+  } = {},
+): Promise<{
+  outputFiles: string[]
+  outputDir: string
+  getContent: (filename: string) => Promise<string>
+  outputContent: () => Promise<string>
+}> {
+  const testDir = getTestDir()
+  await mkdir(testDir, { recursive: true })
+
+  for (const [filename, content] of Object.entries(files)) {
+    await writeFile(path.resolve(testDir, filename), content, 'utf8')
+  }
+
+  await execa({
+    cwd: testDir,
+    env: { CONSOLA_LEVEL: '3' },
+  })`${tsx} ${run} ${entry} ${config ? '' : '--no-config'} -d ${outDir} ${args}`.catch()
+
+  const outputDir = path.resolve(testDir, outDir)
+  const outputFiles = await new fdir()
+    .withRelativePaths()
+    .crawl(outputDir)
+    .withPromise()
+
+  const getContent = (filename: string) =>
+    readFile(path.resolve(outputDir, filename), 'utf8')
+
+  return {
+    outputFiles,
+    outputDir,
+    getContent,
+    outputContent: () => getContent('index.mjs'),
+  }
+}
+
+function getTestName(): string {
+  const name = expect.getState().currentTestName
+  if (!name) {
+    throw new Error('No test name')
+  }
+  return name
+}
+
+function filenamify(input: string) {
+  return input.replaceAll(/[^\dA-Za-z]/g, '-')
+}
