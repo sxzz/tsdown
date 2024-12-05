f801643b1e3aba6cc49173dcfecec21102cbd8db
三咲智子 Kevin Deng <sxzz@sxzz.moe>
2024-12-05 23:52:45 +0800

    feat: keep entry structure

--- src/features/entry.ts
+++ src/features/entry.ts
@@ -1,6 +1,7 @@
+import path from 'node:path'
 import pc from 'picocolors'
 import { glob } from 'tinyglobby'
-import { fsExists } from '../utils/fs'
+import { fsExists, lowestCommonAncestor } from '../utils/fs'
 import { logger } from '../utils/logger'
 import type { Options } from '../options'
 
@@ -17,11 +18,20 @@ export async function resolveEntry(
   }
   if (Array.isArray(entry)) {
     const resolvedEntry = await glob(entry)
+    const base = lowestCommonAncestor(...resolvedEntry)
 
     // Ensure entry exists
     if (resolvedEntry.length > 0) {
-      entry = resolvedEntry
-      logger.info(`entry: ${pc.blue(entry.join(', '))}`)
+      logger.info(`entry: ${pc.blue(resolvedEntry.join(', '))}`)
+      entry = Object.fromEntries(
+        resolvedEntry.map((file) => {
+          const relative = path.relative(base, file)
+          return [
+            relative.slice(0, relative.length - path.extname(relative).length),
+            file,
+          ]
+        }),
+      )
     } else {
       throw new Error(`Cannot find entry: ${entry}`)
     }
--- src/utils/fs.ts
+++ src/utils/fs.ts
@@ -1,8 +1,34 @@
 import { access } from 'node:fs/promises'
+import { dirname, normalize, sep } from 'node:path'
 
 export function fsExists(path: string): Promise<boolean> {
   return access(path).then(
     () => true,
     () => false,
   )
+}
+
+export function lowestCommonAncestor(...filepaths: string[]): string {
+  if (filepaths.length === 0) return ''
+  if (filepaths.length === 1) return dirname(filepaths[0])
+  filepaths = filepaths.map(normalize)
+  const [first, ...rest] = filepaths
+  let ancestor = first.split(sep)
+  for (const filepath of rest) {
+    const directories = filepath.split(sep, ancestor.length)
+    let index = 0
+    for (const directory of directories) {
+      if (directory === ancestor[index]) {
+        index += 1
+      } else {
+        ancestor = ancestor.slice(0, index)
+        break
+      }
+    }
+    ancestor = ancestor.slice(0, index)
+  }
+
+  return ancestor.length <= 1 && ancestor[0] === ''
+    ? sep + ancestor[0]
+    : ancestor.join(sep)
 }
