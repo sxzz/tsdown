3be9fd3495cc8d62537c1c69827847f9dc4fac6e
三咲智子 Kevin Deng <sxzz@sxzz.moe>
2025-01-28 06:11:04 +0800

    refactor: add debug info for publint

--- src/features/publint.ts
+++ src/features/publint.ts
@@ -1,11 +1,16 @@
 import process from 'node:process'
+import Debug from 'debug'
 import { publint as _publint } from 'publint'
 import { formatMessage } from 'publint/utils'
 import { logger } from '../utils/logger'
 import type { PackageJson } from 'pkg-types'
 
+const debug = Debug('tsdown:publint')
+
 export async function publint(pkg: PackageJson): Promise<void> {
+  debug('Running publint')
   const { messages } = await _publint()
+  debug('Found %d issues', messages.length)
   if (!messages.length) {
     logger.success('No publint issues found')
   }
@@ -19,6 +24,7 @@ export async function publint(pkg: PackageJson): Promise<void> {
     logger[logType](formattedMessage)
   }
   if (hasError) {
+    debug('Found errors, setting exit code to 1')
     process.exitCode = 1
   }
 }
