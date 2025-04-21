07d534eacc96378e2e9f139dbadaaa0062ff002e
buu <hi@bushuai.cc>
2025-04-22 03:16:37 +0800

    docs: fix voidzero logo in light mode (#134)
    Co-authored-by: 三咲智子 Kevin Deng <sxzz@sxzz.moe>

--- docs/.vitepress/components/HomePage.vue
+++ docs/.vitepress/components/HomePage.vue
@@ -1,23 +1,23 @@
 <script setup lang="ts">
 import { useData } from 'vitepress'
-import { computed } from 'vue'
 
-const { lang, isDark } = useData()
-
-const voidZeroLogo = computed(() =>
-  isDark
-    ? 'https://voidzero.dev/logo-white.svg'
-    : 'https://voidzero.dev/logo.svg',
-)
+const { lang } = useData()
 </script>
 
 <template>
   <div flex="~ col" items-center justify-center>
     <div mt20 flex="~ col" items-center gap8>
       <h2 v-if="lang === 'en'" class="voidzero-title">Brought to you by</h2>
-      <a href="https://voidzero.dev/" target="_blank" title="voidzero.dev">
-        <img :src="voidZeroLogo" alt="VoidZero" h-75px w-300px />
-      </a>
+      <a
+        class="voidzero"
+        href="https://voidzero.dev/"
+        target="_blank"
+        title="voidzero.dev"
+        alt="VoidZero"
+        block
+        h-75px
+        w-300px
+      />
       <h2 v-if="lang === 'zh-CN'" class="voidzero-title">
         由 VoidZero 隆重推出
       </h2>
@@ -35,5 +35,14 @@ const voidZeroLogo = computed(() =>
   line-height: 32px;
   font-size: 24px;
   font-weight: 600;
+}
+
+.voidzero {
+  background: url(https://voidzero.dev/logo.svg) no-repeat center;
+  background-size: contain;
+}
+
+.dark .voidzero {
+  background-image: url(https://voidzero.dev/logo-white.svg);
 }
 </style>
