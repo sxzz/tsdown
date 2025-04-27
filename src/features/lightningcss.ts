5c0b413447fefa737fc39b851fe569a1abfe5780
三咲智子 Kevin Deng <sxzz@sxzz.moe>
2025-04-27 20:28:54 +0800

    refactor

--- src/features/lightningcss.ts
+++ src/features/lightningcss.ts
@@ -1,25 +1,15 @@
 import LightningCSS from 'unplugin-lightningcss/rolldown'
 import { esbuildTargetToLightningCSS } from '../utils/lightningcss'
-
 import type { ResolvedOptions } from '../options'
-import type { Targets } from 'lightningcss'
 import type { Plugin } from 'rolldown'
 
 export function LightningCSSPlugin(
   options: Pick<ResolvedOptions, 'target'>,
-): Plugin {
+): Plugin | undefined {
   // Converts the user-provided esbuild-format target into a LightningCSS
   // targets object.
-  const targets: Targets | undefined =
-    options.target && esbuildTargetToLightningCSS(options.target)
+  const targets = options.target && esbuildTargetToLightningCSS(options.target)
+  if (!targets) return
 
   return LightningCSS({ options: { targets } })
-}
-
-/**
- * Whether the target contains browsers like `chrome`, `firefox`, `safari`, etc
- * that affect the CSS transpilation.
- */
-export function hasCSSTarget(target: ResolvedOptions['target']): boolean {
-  return !!(target && esbuildTargetToLightningCSS(target))
 }
