1528d0559b7da0b6a824ac897c3106d98a79833c
ocavue <ocavue@gmail.com>
2025-04-27 20:10:18 +1000

    add comment

--- src/utils/lightningcss.test.ts
+++ src/utils/lightningcss.test.ts
@@ -5,20 +5,25 @@ import { esbuildTargetToLightningCSS } from './lightningcss'
 test('esbuildTargetToLightningCSS', () => {
   const expected = browserslistToTargets([
     'chrome 99',
-    'safari 16.2.1',
-    'firefox 120.1',
+    'safari 16.2',
+    'firefox 120.1.2',
   ])
   const actual = esbuildTargetToLightningCSS([
+    // A browser version with only a major version.
     'chrome99',
-    'safari16.2.1 firefox120.1',
+    // A browser version with a major and minor version.
+    'safari16.2 ' +
+      // A browser version with a major, minor, and patch version.
+      'firefox120.1.2',
+    // No browser.
     'node12 es2021',
   ])
 
   expect(actual).toMatchInlineSnapshot(`
     {
       "chrome": 6488064,
-      "firefox": 7864576,
-      "safari": 1049089,
+      "firefox": 7864578,
+      "safari": 1049088,
     }
   `)
   expect(actual).toEqual(expected)
