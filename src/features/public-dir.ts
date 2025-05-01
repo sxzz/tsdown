a7a004ee5ef33e7797c75c129338e7fcde5023e5
三咲智子 Kevin Deng <sxzz@sxzz.moe>
2025-05-01 13:24:01 +0800

    copy

--- src/features/public-dir.ts
+++ src/features/public-dir.ts
@@ -1,15 +1,38 @@
-import type { NormalizedFormat, ResolvedOptions } from '../options'
+import path from 'node:path'
+import { fsCopy } from '../utils/fs'
+import { toArray } from '../utils/general'
+import type { ResolvedOptions } from '../options'
 import type { Arrayable, Awaitable } from '../utils/types'
 
-export type PublicDir = Arrayable<string> | Record<string, string>
-export interface PublicDirContext {
-  outDir: string
-  format: NormalizedFormat
-  options: ResolvedOptions
+export interface PublicDirEntry {
+  from: string
+  to: string
 }
-export type PublicDirFn = () => Awaitable<PublicDir>
+export type PublicDir = Arrayable<string | PublicDirEntry>
+export type PublicDirFn = (options: ResolvedOptions) => Awaitable<PublicDir>
 
-export function copyPublicDir(config: ResolvedOptions): void {
-  if (!config.publicDir) return
-  //
+export async function copyPublicDir(options: ResolvedOptions): Promise<void> {
+  if (!options.publicDir) return
+
+  let publicDir: PublicDir
+  if (typeof options.publicDir === 'function') {
+    publicDir = await options.publicDir(options)
+  } else {
+    publicDir = options.publicDir
+  }
+
+  await Promise.all(
+    toArray(publicDir).map((dir) => {
+      const from = typeof dir === 'string' ? dir : dir.from
+      const to =
+        typeof dir === 'string'
+          ? path.resolve(options.outDir, path.basename(from))
+          : dir.to
+      return copy(options.cwd, from, to)
+    }),
+  )
+}
+
+function copy(cwd: string, from: string, to: string): Promise<void> {
+  return fsCopy(path.resolve(cwd, from), path.resolve(cwd, to))
 }
