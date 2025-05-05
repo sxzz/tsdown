daaac8d6773e25579d096ef23ccb720ee97f5d4c
三咲智子 Kevin Deng <sxzz@sxzz.moe>
2025-05-05 21:20:39 +0800

    refactor: remove `defineConfig` overload
    When type doesn't match, it's difficult to understand the cause of the error

--- src/config.ts
+++ src/config.ts
@@ -3,8 +3,6 @@ import type { UserConfig, UserConfigFn } from './options'
 /**
  * Defines the configuration for tsdown.
  */
-export function defineConfig(options: UserConfig): UserConfig
-export function defineConfig(options: UserConfigFn): UserConfigFn
 export function defineConfig(
   options: UserConfig | UserConfigFn,
 ): UserConfig | UserConfigFn {
