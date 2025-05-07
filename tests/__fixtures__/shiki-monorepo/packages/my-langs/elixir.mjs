f49d4e7f3e4a7cc721319968c8b93524b589ff57
ocavue <ocavue@gmail.com>
2025-05-07 11:29:29 +1000

    rename html

--- tests/__fixtures__/shiki-monorepo/packages/my-langs/elixir.mjs
+++
@@ -1,2 +0,0 @@
-import css from './css.mjs'
-export default [...css, 'ELIXIR']
--- tests/__fixtures__/shiki-monorepo/packages/my-shiki/index.mjs
+++ tests/__fixtures__/shiki-monorepo/packages/my-shiki/index.mjs
@@ -1,4 +1,4 @@
-const langs = [() => import('my-langs/css'), () => import('my-langs/elixir')]
+const langs = [() => import('my-langs/css'), () => import('my-langs/html')]
 
 export const codeToHtml = async (code) => {
   return String(langs) + code
