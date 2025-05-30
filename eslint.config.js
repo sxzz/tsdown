d08fcd0c603f0f8bb3aee2d6a8bb94588de2ef1b
BitterGourd <91231822+gaoachao@users.noreply.github.com>
2025-05-30 18:45:49 +0800

    chore: add simple example (#283)
    Co-authored-by: 三咲智子 Kevin Deng <sxzz@sxzz.moe>

--- eslint.config.js
+++ eslint.config.js
@@ -2,4 +2,9 @@ import { sxzz } from '@sxzz/eslint-config'
 
 export default sxzz({
   pnpm: true,
+}).append({
+  files: ['examples/**'],
+  rules: {
+    'pnpm/json-enforce-catalog': 'off',
+  },
 })
