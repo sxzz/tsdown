f84eeb6b4ca1e46777ed4967a2ca307047c220ae
BitterGourd <91231822+gaoachao@users.noreply.github.com>
2025-05-30 18:37:52 +0800

    chore: remove dts option & use single config

--- examples/default/tsdown.config.ts
+++ examples/default/tsdown.config.ts
@@ -1,9 +1,6 @@
 import { defineConfig } from 'tsdown'
 
-export default defineConfig([
-  {
-    entry: ['./src/index.ts'],
-    platform: 'neutral',
-    dts: true,
-  },
-])
+export default defineConfig({
+  entry: ['./src/index.ts'],
+  platform: 'neutral',
+})
