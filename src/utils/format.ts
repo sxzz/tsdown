b491df9b2634b4df84a586aeb67b3b1d2d8cecc4
三咲智子 Kevin Deng <sxzz@sxzz.moe>
2025-06-03 02:35:21 +0800

    perf: remove Intl

--- src/utils/format.ts
+++ src/utils/format.ts
@@ -1,9 +1,4 @@
 export function formatBytes(bytes: number): string | undefined {
   if (bytes === Infinity) return undefined
-  const numberFormatter = new Intl.NumberFormat('en', {
-    maximumFractionDigits: 2,
-    minimumFractionDigits: 2,
-  })
-
-  return `${numberFormatter.format(bytes / 1000)} kB`
+  return `${(bytes / 1000).toFixed(2)} kB`
 }
