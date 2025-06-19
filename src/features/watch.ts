14759cdfedea6b7134bb8d95b11cf43c463fdb25
三咲智子 Kevin Deng <sxzz@sxzz.moe>
2025-06-19 10:41:59 +0800

    feat!: drop chokidar

--- src/features/watch.ts
+++ src/features/watch.ts
@@ -1,56 +1,27 @@
-import { debounce, resolveComma, toArray } from '../utils/general'
 import { logger } from '../utils/logger'
-import type { ResolvedOptions } from '../options'
 import type { Plugin } from 'rolldown'
 
 const endsWithConfig = /[\\/](?:package\.json|tsdown\.config.*)$/
 
-export async function watchBuild(
-  options: ResolvedOptions,
+export function WatchPlugin(
   configFiles: string[],
-  rebuild: () => void,
   restart: () => void,
-): Promise<AsyncDisposable> {
-  const files = resolveComma(
-    toArray(typeof options.watch === 'string' ? options.watch : []),
-  )
-  files.push(...configFiles)
-  logger.info(`Watching for changes...`)
-
-  const { watch } = await import('chokidar')
-  const debouncedRebuild = debounce(rebuild, 100)
-
-  const watcher = watch(files, {
-    ignoreInitial: true,
-    ignorePermissionErrors: true,
-    ignored: [
-      /[\\/]\.git[\\/]/,
-      /[\\/]node_modules[\\/]/,
-      ...toArray(options.ignoreWatch),
-    ],
-  })
-
-  watcher.on('all', (type: string, file: string) => {
-    if (configFiles.includes(file) || endsWithConfig.test(file)) {
-      logger.info(`Reload config: ${file}`)
-      restart()
-      return
-    }
-
-    logger.info(`Change detected: ${type} ${file}`)
-    debouncedRebuild()
-  })
-
-  return {
-    [Symbol.asyncDispose]: () => watcher.close(),
-  }
-}
-
-export function WatchPlugin(): Plugin {
+): Plugin {
   return {
     name: 'tsdown:watch',
-    watchChange(id) {
-      logger.info(`Change detected: ${id}`)
+    buildStart() {
+      for (const file of configFiles) {
+        this.addWatchFile(file)
+      }
+    },
+    watchChange(id, event) {
+      if (configFiles.includes(id) || endsWithConfig.test(id)) {
+        logger.info(`Reload config: ${id}`)
+        restart()
+      } else {
+        console.info('')
+        logger.info(`File ${event.event}d: ${id}`)
+      }
     },
   }
 }
