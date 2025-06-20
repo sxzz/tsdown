25db3426343c0c21a2cf6664b584813849a70db5
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2025-06-20 13:37:07 +0100

    feat: resolve removeNodeProtocol and removeNodeProtocol

--- src/features/node-protocol.ts
+++ src/features/node-protocol.ts
@@ -1,10 +1,19 @@
+import type { ResolvedOptions } from '..'
 import type { Plugin } from 'rolldown'
 
 /**
  * The `node:` protocol was added in Node.js v14.18.0.
  * @see https://nodejs.org/api/esm.html#node-imports
  */
-export function NodeProtocolPlugin(): Plugin {
+export function NodeProtocolPlugin(
+  options: Pick<ResolvedOptions, 'nodeProtocol' | 'removeNodeProtocol'>,
+): Plugin {
+  const nodeProtocolOption =
+    options.nodeProtocol ??
+    // `removeNodeProtocol: true` means stripping the `node:` protocol which equales to `nodeProtocol: 'strip'`
+    // `removeNodeProtocol: false` means keeping the `node:` protocol which equales to `nodeProtocol: false` (ignore it)
+    (options.removeNodeProtocol ? 'strip' : false)
+
   return {
     name: 'tsdown:node-protocol',
     resolveId: {
